491c907ece38ce3761b963dbc69cfb3766914125
kumarjeetray <53223216+kumarjeetray@users.noreply.github.com>
2019-07-25 08:05:50 +0530

    Add files via upload

---
+++ Pattern6.go
@@ -0,0 +1,21 @@
+/*
+ABCDE
+BCDE
+CDE
+DE
+E
+*/
+package main
+import(
+	 "fmt"
+)
+func main(){
+	n:=65
+	for i:=1;i<=5;i++{
+		for j:=1;j<=i;j++{
+			fmt.Print(string(j+64))
+		}
+		fmt.Println()
+		n++
+	}
+}
