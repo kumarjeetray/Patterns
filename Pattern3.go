eba2a01f43619d1207410a6fc49e6c93f620ab56
kumarjeetray <53223216+kumarjeetray@users.noreply.github.com>
2019-07-25 07:47:12 +0530

    Add files via upload

---
+++ Pattern3.go
@@ -0,0 +1,22 @@
+/*              1
+			   2 2
+			  3 3 3
+			 4 4 4 4
+			5 5 5 5 5
+		*/
+		package main
+		import "fmt"
+		func main(){
+			a:=4
+			for i:=1;i<=5;i++{
+				for k:=a;k>0;k--{
+					fmt.Print(" ")
+				}
+				for j:=1;j<=i;j++{
+					fmt.Print(i)
+					fmt.Print(" ")
+				}
+				a--
+				fmt.Println()
+			}
+		}
