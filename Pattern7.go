9eecaee8de41cb8acc63b26fcb43a05a9e7ba04a
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-07-26 05:52:08 +0530

    Add files via upload

---
+++ Pattern7.go
@@ -0,0 +1,27 @@
+/*   1234
+	 2341
+	 3421
+	 4321
+*/
+package main
+import "fmt"
+func main(){
+	c:=1
+	d:=0
+	j:=0
+	for i:=1;i<=4;i++{
+		for j=c;j<=4;j++{
+			fmt.Print(j)
+		}
+		if i>1 {
+		for k:=d;k>=1;k--{
+			fmt.Print(k)
+		}
+		}
+		c++
+		d++
+		fmt.Println()
+	}
+}
+
+
