11ca0bc8ff9bb77b1fe78576a5b6d156f1953978
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-07-28 20:40:45 +0530

    Update Pattern19.go

--- Pattern19.go
+++ Pattern19.go
@@ -2,34 +2,34 @@ package main
 import "fmt"
 func main(){
 	n:=7  
-		for i:=1; i<=n; i++{ 
-			for j:=1; j<=(2*n); j++{ 
-				if (i>(n-j+1)){ 
-					fmt.Print(" ")
-				}else{
-					fmt.Print("*")
-				}
-				if ((i+n)>j){ 
-					fmt.Print(" ") 
-				}else{
-					fmt.Printf("*")
-				} 
-			} 
-			fmt.Println() 
-		} 
-		for i:=1; i<=n; i++{ 
-			for j:=1; j<=(2*n); j++{  	 
-				if (i<j){
-					fmt.Print(" ")
-				}else{
-					fmt.Print("*")
-			}
-				if (i<=((2*n)-j)){ 
-					fmt.Print(" ") 
-				}else{
-					fmt.Print("*") 
-			}
-		}
-			fmt.Println()
+	for i:=1; i<=n; i++{ 
+	for j:=1; j<=(2*n); j++{ 
+	if (i>(n-j+1)){ 
+	fmt.Print(" ")
+	}else{
+	fmt.Print("*")
+	}
+	if ((i+n)>j){ 
+	fmt.Print(" ") 
+	}else{
+	fmt.Printf("*")
+	} 
+	} 
+	fmt.Println() 
+	} 
+	for i:=1; i<=n; i++{ 
+	for j:=1; j<=(2*n); j++{  	 
+	if (i<j){
+	fmt.Print(" ")
+	}else{
+	fmt.Print("*")
+	}
+	if (i<=((2*n)-j)){ 
+	fmt.Print(" ") 
+	}else{
+	fmt.Print("*") 
+	}
+	}
+	fmt.Println()
 	} 
 }
