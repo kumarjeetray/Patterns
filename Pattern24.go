5ca43187d1d7246870f12aa9da82f9f85dd3f197
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-07-29 22:21:13 +0530

    Add files via upload

---
+++ Pattern24.go
@@ -0,0 +1,30 @@
+/*
+ ******
+ *     *
+ *     *
+ *     *
+ *     *
+ *     *
+ *     *
+ *     *
+ ******
+ 
+ */
+ package main
+import "fmt"
+func main(){
+	n:=9
+	for i:= 0; i < n; i++{  
+        for j:= 0; j <= n; j++{ 
+            if (j == 1 || ((i == 0 || i == n-1) && 
+               (j > 1 && j < n-2)) || (j == n-2 && 
+                i != 0 && i != n-1)){
+                fmt.Print("*")  
+                }else{
+                fmt.Print(" ") 
+                }
+        } 
+          
+    fmt.Println()
+    } 
+}
