3bc5ac085c0a97050b9961016d6b886859e31dc1
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-07-30 22:27:08 +0530

    Add files via upload

---
+++ Pattern26.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+func main(){
+	length:=25
+	height:=12
+    for i:= 0; i < height; i++ { 
+        for j:= 0; j < length; j++{ 
+            if i == 0{ 
+                if (j == 0 || j == height || j == length - 1){ 
+                   fmt.Print("*")
+                }else{ 
+                    fmt.Print(" ")
+			}
+
+			}else if i == height- 1{ 
+                fmt.Print("*") 
+            }else if ((j < i || j > height - i) &&(j < height + i || j >= length - i)){ 
+				fmt.Print("#")
+			}else{
+				fmt.Print(" ") 
+			}
+        } 
+       fmt.Println() 
+    } 
+} 
